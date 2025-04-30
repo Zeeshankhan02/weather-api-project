70a728dea5b4cc12f47b27a7522dc69bef7a0c10
Zeeshan Khan <zeeshankhan0271@gmail.com>
2025-04-30 12:50:30 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from "express"
+import "dotenv/config"
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+  origin:"*"
+}))
+const PORT = process.env.PORT || 3002
+
+async function fetchWeatherData(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}`;
+
+  const response = await fetch(url);
+
+  const data = await response.json();
+  return data;
+}
+
+app.get('/', async (req, res) => {
+  const { city } = req.query; // ✅ fixed
+  if (!city) {
+    return res.status(400).json({ error: "City is required" });
+  }
+
+  try {
+    const data = await fetchWeatherData(city);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch weather data" });
+  }
+});
+
+
+app.listen(PORT,()=>{
+  console.log(`Server Running on port ${PORT}`);
+})
---
+++ frontend/index.js
@@ -0,0 +1,60 @@
+const cityInput = document.getElementById("city-input");
+const getWheatherBtn = document.getElementById("get-weather-btn");
+const weatherInfo = document.getElementById("weather-info");
+const cityNameDisplay = document.getElementById("city-name");
+const temperatureDisplay = document.getElementById("temperature");
+const descriptionDisplay = document.getElementById("description");
+const errorMessage = document.getElementById("error-message");
+
+const handleWeatherRequest=async () => {
+  if (cityInput.value === "") return;
+  const city = cityInput.value.trim();
+  try {
+    const data = await fetchWeatherData(city);
+    displayData(data);
+  } catch (error) {
+    showError()
+  }
+}
+
+getWheatherBtn.addEventListener("click",handleWeatherRequest);
+
+cityInput.addEventListener('keydown',(e)=>{
+  if(e.key=='Enter'){
+  handleWeatherRequest()}
+}
+)
+
+async function fetchWeatherData(city) {
+  try {
+    const response = await fetch(`http://localhost:3001/?city=${encodeURIComponent(city)}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data
+  } catch (error) {
+    console.error("Failed to fetch data:", error.message);
+  }
+}
+
+
+
+function displayData(data) {
+  const { name, main, weather } = data;
+  cityNameDisplay.textContent = name;
+  temperatureDisplay.textContent = `
+    Temperature: ${main.temp} degrees
+    `;
+  descriptionDisplay.textContent = weather[0].description;
+  cityInput.value=''
+  weatherInfo.classList.remove("hidden");
+  errorMessage.classList.add("hidden");
+}
+
+function showError(){
+  weatherInfo.classList.add("hidden");
+  errorMessage.classList.remove("hidden");
+}
