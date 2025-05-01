c3c83c190b19b098a16a52afeb99e8e8ebbfd2a7
Zeeshan Khan <142289960+Zeeshankhan02@users.noreply.github.com>
2025-05-01 16:28:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ app.get("/health", (req, res) => {
 
 
 app.get('/', async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://zeeshankhan02.github.io");
   const { city } = req.query; // ✅ fixed
   if (!city) {
   return res.status(400).json({ error: "City is required" });
